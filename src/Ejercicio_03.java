df8a0efd0cca05a0b455bb7d1b5e82efd78ea37e
Franco <juafracas@alu.edu.gva.es>
2024-10-17 17:56:01 +0200

    Inicio Ejercicio_03

--- src/Ejercicio_03.java
+++ src/Ejercicio_03.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Ejercicio_03 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        int mayor = Integer.MIN_VALUE;
+        int menor = Integer.MAX_VALUE;
+
+        System.out.println("Introduce numero, introduce 0 para finalizar.\nIngresa un numero: ");
+        num = scan.nextInt();
+
+
+        while (num != 0) {
+            if (num > mayor) {
+                mayor = num;
+            }
+            if (num < menor) {
+                menor = num;
+            }
+            System.out.print("Ingresa un numero: ");
+            num = scan.nextInt();
+        }
+
+        if (mayor != Integer.MIN_VALUE && menor != Integer.MAX_VALUE) {
+            System.out.println("El numero mayor es: " + mayor);
+            System.out.println("El numero menor es: " + menor);
+        } else {
+            System.out.println("No ingresaste ningún numero valido.");
+        }
 
     }
 }
